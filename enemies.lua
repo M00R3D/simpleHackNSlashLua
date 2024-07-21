3d01832e9e2b13527c41bb5372604279a2f03e21
M00R3D <jobmurdan@hotmail.com>
2024-07-21 00:21:26 -0700

    he añadido los drops de experiencia

--- enemies.lua
+++ enemies.lua
@@ -1,91 +1,115 @@
---enemies.lua
 local Variables = require("variables")
 local Physics = require("physics")
------------------creacion de arreglo de enemigos----------------------------
-local Enemies={}
------------------creacion de arreglo de enemigos----------------------------
 
+-----------------creacion de arreglo de enemigos----------------------------
+local Enemies = {}
+Enemies.enemies = {}  -- Inicializa la tabla de enemigos
+Enemies.xps = {}      -- Inicializa la tabla de XP
+-----------------creacion de arreglo de enemigos----------------------------
 
 -----------------creacion de un enemigo----------------------------
-    function Enemies.createEnemy(x, y)
-        enemy = 
-        {
-            image = love.graphics.newImage("sprites/sprite_enemy_a/sprite_enemy_a0.png"),
-            x=x,
-            y=y,
-            width=30,
-            height=30,
-            speed=2,
-            life=30
-        }    
-        table.insert( enemies, enemy )
-    end
+function Enemies.createEnemy(x, y)
+    local enemy = {
+        image = love.graphics.newImage("sprites/sprite_enemy_a/sprite_enemy_a0.png"),
+        x = x,
+        y = y,
+        width = 30,
+        height = 30,
+        speed = 2,
+        life = 30,
+        destroyed = false
+    }    
+    table.insert(Enemies.enemies, enemy)
+end
+
+function Enemies.createXp(x, y)
+    local xp = {
+        image = love.graphics.newImage("sprites/Xp.png"),
+        x = x,
+        y = y,
+        width = 30,
+        height = 30
+    }    
+    table.insert(Enemies.xps, xp)
+end
 -----------------creacion de un enemigo----------------------------
 
-
-
 -----------------dibujar todo el arreglo de enemigos----------------------------
-    function Enemies.drawEnemies()
-        for i, enemy in ipairs(enemies) do
-            love.graphics.draw(enemy.image, enemy.x, enemy.y)
-        end
+function Enemies.drawEnemies()
+    for _, enemy in ipairs(Enemies.enemies) do
+        love.graphics.draw(enemy.image, enemy.x, enemy.y)
     end
------------------dibujar todo el arreglo de enemigos----------------------------
+end
 
+function Enemies.drawXp()
+    for _, xp in ipairs(Enemies.xps) do
+        love.graphics.draw(xp.image, xp.x, xp.y)
+    end
+end
+-----------------dibujar todo el arreglo de enemigos----------------------------
 
 ----movimiento
-    function Enemies.updateEnemies(dt)
-        for i, enemy in ipairs(enemies) do
-            if enemy.life <=0 then enemy.destroyed= true end
-            if enemy.destroyed then
-                table.remove(enemies, i)
-            else
-                -- Manejar la colisión con otros enemigos
-                for j, otherEnemy in ipairs(enemies) do
-                    if i ~= j and Physics.checkCollision(enemy, otherEnemy) then
-                        -- Aplicar movimiento opuesto
-                        if enemy.x < otherEnemy.x then
-                            enemy.x = enemy.x - enemy.speed
-                        elseif enemy.x > otherEnemy.x then
-                            enemy.x = enemy.x + enemy.speed
-                        end
-                        if enemy.y < otherEnemy.y then
-                            enemy.y = enemy.y - enemy.speed
-                        elseif enemy.y > otherEnemy.y then
-                            enemy.y = enemy.y + enemy.speed
-                        end
+function Enemies.updateXp(dt)
+    for i = #Enemies.xps, 1, -1 do
+        local xp = Enemies.xps[i]
+        if Physics.checkCollision(player, xp) then
+            table.remove(Enemies.xps, i)
+            player.exp=player.exp+5
+        end
+    end
+end
+
+function Enemies.updateEnemies(dt)
+    for i = #Enemies.enemies, 1, -1 do
+        local enemy = Enemies.enemies[i]
+
+        if enemy.life <= 0 then
+            Enemies.createXp(enemy.x, enemy.y)
+            table.remove(Enemies.enemies, i)
+        else
+            -- Manejar la colisión con otros enemigos
+            for j, otherEnemy in ipairs(Enemies.enemies) do
+                if i ~= j and Physics.checkCollision(enemy, otherEnemy) then
+                    -- Aplicar movimiento opuesto
+                    if enemy.x < otherEnemy.x then
+                        enemy.x = enemy.x - enemy.speed
+                    elseif enemy.x > otherEnemy.x then
+                        enemy.x = enemy.x + enemy.speed
+                    end
+                    if enemy.y < otherEnemy.y then
+                        enemy.y = enemy.y - enemy.speed
+                    elseif enemy.y > otherEnemy.y then
+                        enemy.y = enemy.y + enemy.speed
                     end
                 end
-                -- Manejar la colisión con el jugador
-                if Physics.checkCollision(player, enemy) then
-                    -- Aplicar rebote suave al jugador
-                    local dx = player.x - enemy.x
-                    local dy = player.y - enemy.y
-                    local distance = math.sqrt(dx*dx + dy*dy)
-                    local penetration = player.width/2 + enemy.width/2 - distance
-                    local normalX = dx / distance
-                    local normalY = dy / distance
-                    player.x = player.x + normalX * penetration * 0.5
-                    player.y = player.y + normalY * penetration * 0.5
-                    Variables.damagePlayer(3)
-                end
-                -- Mover a los enemigos según su dirección
-                -- Esto debería estar fuera del bucle de colisión para evitar movimientos adicionales
-                -- causados por la detección de colisiones.
-                if enemy.x < player.x then
-                    enemy.x = enemy.x + enemy.speed
-                elseif enemy.x > player.x then
-                    enemy.x = enemy.x - enemy.speed
-                end
-                if enemy.y < player.y then
-                    enemy.y = enemy.y + enemy.speed
-                elseif enemy.y > player.y then
-                    enemy.y = enemy.y - enemy.speed
-                end
+            end
+
+            -- Manejar la colisión con el jugador
+            if Physics.checkCollision(player, enemy) then
+                local dx = player.x - enemy.x
+                local dy = player.y - enemy.y
+                local distance = math.sqrt(dx*dx + dy*dy)
+                local penetration = player.width/2 + enemy.width/2 - distance
+                local normalX = dx / distance
+                local normalY = dy / distance
+                player.x = player.x + normalX * penetration * 0.5
+                player.y = player.y + normalY * penetration * 0.5
+                Variables.damagePlayer(3)
+            end
+
+            -- Mover a los enemigos
+            if enemy.x < player.x then
+                enemy.x = enemy.x + enemy.speed
+            elseif enemy.x > player.x then
+                enemy.x = enemy.x - enemy.speed
+            end
+            if enemy.y < player.y then
+                enemy.y = enemy.y + enemy.speed
+            elseif enemy.y > player.y then
+                enemy.y = enemy.y - enemy.speed
             end
         end
     end
-    
+end
 
-    
 return Enemies
--- main.lua
+++ main.lua
@@ -11,68 +11,67 @@ function love.load()
     cam:zoom(1.4)
 
     background = love.graphics.newImage("sprites/background.png")
-
 end
-
-
 
 function love.update(dt)
     if player.isDead then
         cam:zoom(3)
         return 
     end
-    Input.playerInputs()---cargar controles
-    Physics.playerControl()----cargar movimiento jugador
-    Physics.colisionPlayerScreen(player)---detectar que el jugador no se salga de la pantalla
-
+    Input.playerInputs()
+    Physics.playerControl()
+    Physics.colisionPlayerScreen(player)
     Variables.drawPlayerLife()
-    enemyTime=enemyTime-1   ----se reduce el temporizador de spawnerde enemigos
-    
-    Enemies.updateEnemies(dt)------ movimiento de los enemigos y colisionar
-    Variables.playerAnimator(dt)---logica animacion del personaje
-    Proyectiles.updateProyectiles(dt)---- mover proyectiles y colisionar
-    ----------------spawner de enemigos----------------------------
-    if enemyTime==0 then
-        enemyTime=240
-        if enemyCounter<11 then
-            Enemies.createEnemy(200,130)
-            enemyCounter=enemyCounter+1
+
+    enemyTime = enemyTime - 1
+
+    Enemies.updateEnemies(dt)  -- Actualiza enemigos y XP
+    Enemies.updateXp(dt)
+    Variables.playerAnimator(dt)
+    Proyectiles.updateProyectiles(dt)
+
+    -- Spawner de enemigos
+    if enemyTime == 0 then
+        enemyTime = 240
+        if enemyCounter < 11 then
+            Enemies.createEnemy(200, 130)
+            enemyCounter = enemyCounter + 1
         end
     end
-    ----------------spawner de enemigos----------------------------
-    cam:lookAt(player.x,player.y)
-    
+
+    cam:lookAt(player.x, player.y)
+
     local w = love.graphics.getWidth()
     local h = love.graphics.getHeight()
 
-    if cam.x< w/2 then
-        cam.x = w/2
+    if cam.x < w / 2 then
+        cam.x = w / 2
     end
-    if cam.y< h/2 then
-        cam.y = h/2
+    if cam.y < h / 2 then
+        cam.y = h / 2
     end
 end
 
 function love.draw()
-
     local screenWidth = love.graphics.getWidth()
     local screenHeight = love.graphics.getHeight()
 
     -- Dibujar la imagen del fondo escalada para cubrir toda la pantalla
     cam:attach()
         love.graphics.draw(background, 0, 0, 0, screenWidth / background:getWidth(), screenHeight / background:getHeight())
-        love.graphics.draw(player.imageActual,player.x,player.y)
-        love.graphics.draw(player.imageGun,player.x+20+player.gunToogleX,player.y+10)
-        -- love.graphics.draw(player.image, love.mouse.getPosition())
+        love.graphics.draw(player.imageActual, player.x, player.y)
+        love.graphics.draw(player.imageGun, player.x + 20 + player.gunToogleX, player.y + 10)
         Enemies.drawEnemies()
+        Enemies.drawXp()
         Proyectiles.drawProyectiles()
     cam:detach()
-    if player.isDead ==false then
+
+    if not player.isDead then
         Variables.drawPlayerLife()
         Variables.drawPlayerMana()
         Variables.drawPlayerExp()
     else
-        love.graphics.printf("ESTAS MUERTO", 0, screenHeight / 2 - 10, screenWidth, "center")
+        love.graphics.printf("ESTÁS MUERTO", 0, screenHeight / 2 - 10, screenWidth, "center")
     end
 end
 
@@ -80,24 +79,15 @@ function love.mousepressed(x, y, button)
     if button == 1 then
         -- Convertir las coordenadas del mouse a las coordenadas del mundo
         local worldX, worldY = cam:worldCoords(x, y)
-        
+
         -- Calcular el ángulo entre el jugador y el mouse
         local angle = math.atan2(worldY - player.y, worldX - player.x)
-        
+
         -- Calcular direcciones X y Y a partir del ángulo
         local directionX = math.cos(angle)
         local directionY = math.sin(angle)
-        
+
         -- Crear un proyectil en la posición del jugador
         Proyectiles.createProyectil(player.x + 20, player.y + 10, directionX, directionY)
     end
 end
-
-
---     function love.mousereleased(x, y, button)
-
---     end
-
--- function love.quit()
-
--- end
